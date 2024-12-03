3a1cb030290da00cbfe24de04e4970a884304792
september-2 <136699345+september-2@users.noreply.github.com>
2024-12-03 19:23:23 +0700

    Add files via upload

---
+++ Java.java
@@ -0,0 +1,20 @@
+
+import java.util.Scanner;
+public class Java {
+    
+        public static void main (String[] args)
+    {
+        int a;
+       
+       
+        Scanner X = new Scanner(System.in); // import java.util.Scanner;
+   
+       System.out.print("Hello world code in java ");
+    
+    
+       a= X.nextInt(); // value = scnr.next()
+   
+System.out.print("SO ban nhap la "+a);
+
+    }
+}
